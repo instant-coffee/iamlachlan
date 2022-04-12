f7ade0087b9e7338dc656e47a9267e53692de2fa
Lachlan <lachlanrm@gmail.com>
2022-04-12 16:31:47 -0700

    style: adds github link logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Title from '@/components/Title';
 import styled from 'styled-components';
 import Layout from '@/components/layout';
+import logo from '../Assets/GitHub-32px.png';
 
 const Container = styled.main`
   margin: 3rem auto;
-  /* display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center; */
+  justify-content: center;
+`;
+
+const Logo = styled.img`
+  margin: 2rem;
 `;
 
 export default function Home() {
@@ -17,6 +22,9 @@ export default function Home() {
       <Container>
         <Title>I am Lachlan</Title>
         <p>I am a Front end developer</p>
+        <a href="https://github.com/instant-coffee">
+          <Logo src={logo} alt="Logo" />
+        </a>
       </Container>
     </Layout>
   );
